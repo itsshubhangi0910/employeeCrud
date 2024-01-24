cbb65d6bfb58ade61cb46160b8dd6ca3f69128e4
itsshubhangi0910 <shubhangi.thaware11@gmail.com>
2024-01-24 16:43:49 +0530

    change in api

--- src/main/java/com/example/employeeProject/controller/EmployeeController.java
+++ src/main/java/com/example/employeeProject/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/apii")
 public class EmployeeController {
 
     @Autowired
